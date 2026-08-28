080702204154d23afc24172056abd79c7786d1a4
EasinTanvir <easintanvir102@gmail.com>
2026-08-28 16:40:36 +0600

    added client wrapper

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,11 +1,12 @@
 import Sidebar from "@/components/layout/Sidebar/Sidebar";
 import Header from "@/components/layout/Header/Header";
 import Footer from "@/components/pages/dashboard/Footer";
+import SidebarWrapper from "@/components/layout/Sidebar/SidebarWrapper";
 
 export default function DashboardLayout({ children }) {
   return (
     <div className="flex h-dvh overflow-hidden bg-page-bg  font-nunito">
-      <Sidebar />
+      <SidebarWrapper />
       <div className="min-w-0 flex flex-1 flex-col overflow-hidden">
         <Header />
         <main className="min-h-0 flex-1 overflow-y-auto p-4 lg:p-6">

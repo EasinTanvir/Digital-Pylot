86e05279cbbac7717e1f52b532947caa3064ec8b
EasinTanvir <easintanvir102@gmail.com>
2026-08-28 21:26:52 +0600

    added error

---
+++ src/app/error.js
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div className="min-h-screen flex-center">
+      <p className="text-rose-700">Something Unexpected Happened</p>
+    </div>
+  );
+};
+
+export default ErrorPage;

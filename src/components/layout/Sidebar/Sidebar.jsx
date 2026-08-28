d215612768f092d1dfb1a6f21da9ae425c198416
EasinTanvir <easintanvir102@gmail.com>
2026-08-28 21:43:13 +0600

    fixed some design issues

--- src/components/layout/Sidebar/Sidebar.jsx
+++ src/components/layout/Sidebar/Sidebar.jsx
@@ -7,9 +7,8 @@ import SidebarSection from "./SidebarSection";
 import { useGlobalContext } from "@/providers/GlobalContext";
 
 export default function Sidebar() {
-  const { isOpen, setIsOpen } = useGlobalContext();
+  const { isOpen, closeSidebar } = useGlobalContext();
 
-  const closeSidebar = () => setIsOpen(!isOpen);
   const navigation = getSidebarNavigation();
 
   return (
@@ -19,13 +18,13 @@ export default function Sidebar() {
           type="button"
           aria-label="Close navigation"
           onClick={closeSidebar}
-          className="fixed inset-0 z-30 bg-gray-overlay md:hidden"
+          className="fixed inset-0 z-30 bg-gray-overlay lg:hidden"
         />
       )}
 
       <aside
         aria-label="Dashboard navigation"
-        className={`fixed inset-y-0 left-0 z-40 flex w-60 flex-col border-r border-border-100 bg-white transition-transform duration-200 md:static  ${
+        className={`fixed inset-y-0 left-0 z-40 flex w-60 flex-col border-r border-border-100 bg-white transition-transform duration-200 lg:static  ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
--- src/providers/GlobalContext.jsx
+++ src/providers/GlobalContext.jsx
@@ -1,17 +1,38 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const GlobalContext = createContext(null);
 
 export const GlobalProvider = ({ children }) => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeSidebar = () => {
+    if (window.innerWidth <= 1024) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsOpen(window.innerWidth > 1024);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <GlobalContext.Provider
       value={{
         isOpen,
         setIsOpen,
+        closeSidebar,
       }}
     >
       {children}
